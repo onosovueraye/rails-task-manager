1fe4af5b9d5db9acc09c2e7b0f84bb8fd9114414
Onos Ovueraye <onosovueraye@gmail.com>
2023-05-23 14:17:06 +0100

    completed the task manager task

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "tasks", to: "tasks#list"
+
+  get "tasks/new", to: "tasks#new"
+
+  post "tasks", to: "tasks#create"
+
+  delete "tasks/:id", to: "tasks#destroy"
+
+  patch "tasks/:id", to: "tasks#update"
+
+  get "tasks/:id", to: "tasks#find", as: :task
+
+  get "tasks/:id/edit", to: "tasks#edit"
 end
